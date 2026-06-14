fix: update loss calculation and improve debugging output in model training

model_app/model.py
@@ -60,18 +60,21 @@ for i in tqdm(datapreparer.get_list_of_array()):
     optimizer.zero_grad()
     
 
-    print([VocabularyManager(char_max_len).get_vocab_array(a).shape for  a in Tokenizer.tokenize(i)])
+    # print([VocabularyManager(char_max_len).get_vocab_array(a).shape for  a in Tokenizer.tokenize(i)])
     word_representation = [VocabularyManager(char_max_len).get_vocab_array(a) for a in Tokenizer.tokenize(i)] 
     prediction_scores: torch.Tensor = model(word_representation)
+
     
-    loss = criterion(prediction_scores.view(-1, vocab_size), targets.view(-1))
+    # >> prediction_scores.shape
+    # >> torch.Size([11, 1, 1])
+    probability_matrix = torch.full(prediction_scores.shape, .1)
+    loss = criterion(prediction_scores, probability_matrix)
 
 
     loss.backward()
     optimizer.step()
-    
-    print(f"Epoch {epoch+1:02d}/{epochs} - Loss: {loss.item():.4f}")
-    print(i)
+    # print(i)
+    print(f"- Loss: {loss.item():.4f}")
 
 
 

notes/14_bert.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import math
-from benimkutuphanem.src.benimkutuphanem import TensorMonitor
+from rich import print
 
 class MultiHeadAttention(nn.Module):
     def __init__(self, hidden_size, num_heads, dropout=0.1):
@@ -217,7 +217,7 @@ if __name__ == "__main__":
         
         # Loss hesaplamak için tensor boyutlarını (Batch * Seq_Len, Vocab_Size) şeklinde düzleştir
         loss = criterion(outputs.view(-1, vocab_size), labels.view(-1))
-        
+        print(loss)
         # Geri Yayılım (Backward Pass)
         loss.backward()
         optimizer.step()